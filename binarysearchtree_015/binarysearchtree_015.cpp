98904936863bb4ea01186c711d93f8cd8f7434ed
haris494 <hariis.459@gmail.com>
2023-06-29 23:45:41 +0700

    if (ptr != NULL)

--- binarysearchtree_015/binarysearchtree_015.cpp
+++ binarysearchtree_015/binarysearchtree_015.cpp
@@ -77,7 +77,15 @@ public:
 		{
 			cout << "Tree is empty" << endl;
 			return;
-		} 
+		}
+		if (ptr != NULL)
+		{
+			inorder(ptr->leftchild);
+			cout << ptr->info << " ";
+			inorder(ptr->rightchild);
+		}
+	} 
+	
 
 
 
